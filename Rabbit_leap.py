63565c357693891d56107442b44c19d5d864b342
Devrikh <devrikhjatav87@gmail.com>
2024-09-25 15:30:40 +0530

    DFS added

--- Rabbit_leap.py
+++ Rabbit_leap.py
@@ -76,14 +76,44 @@ def bfs(start_state, goal_state):
             while node:                              
                 path.append(node.state)
                 node = node.parent
-            print('Total nodes explored:', nodes_explored)
+            print('Total nodes explored with BFS:', nodes_explored)
             return path[::-1]
         
         for successor in get_successors(node):
                 queue.append(successor)
     
-    print('Total nodes explored:', nodes_explored)
+    print('Total nodes explored with BFS:', nodes_explored)
     return None
+
+def dfs(start_state, goal_state):
+    start_node = Node(start_state)
+    goal_node = Node(goal_state)
+    stack = [start_node]
+    visited = set()
+    nodes_explored = 0
+    
+    while stack:
+        node = stack.pop()
+        if tuple(node.state) in visited:
+            continue
+        visited.add(tuple(node.state))
+        nodes_explored += 1
+        
+        if node.state == list(goal_node.state):   
+            path = []                            
+            
+            while node:                              
+                path.append(node.state)
+                node = node.parent
+            print('Total nodes explored with DFS:', nodes_explored)
+            return path[::-1]
+        
+        for successor in get_successors(node):
+            stack.append(successor)
+    
+    print('Total nodes explored with DFS:', nodes_explored)
+    return None
+
 
 def print_state(state):
     print(' '.join(state))
@@ -93,10 +123,19 @@ def print_state(state):
 start_state = ('E', 'E', 'E', '.', 'W', 'W', 'W')
 goal_state = ('W', 'W', 'W', '.', 'E', 'E', 'E')
 
-solution = bfs(start_state, goal_state)
-if solution:
-    print("Solution found:")
-    for state in solution:
+solution_bfs = bfs(start_state, goal_state)
+solution_dfs=  dfs(start_state, goal_state)
+
+if solution_bfs:
+    print("Solution found with BFS: \n")
+    for state in solution_bfs:
         print_state(state)
 else:
-    print("No solution found.")
+    print("No solution found with BFS.")
+
+if solution_dfs:
+    print("Solution found with DFS: \n")
+    for state in solution_dfs:
+        print_state(state)
+else:
+    print("No solution found with DFS.")
