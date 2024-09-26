eee03a78a4db3c33172f33e2c77c71307b8e107c
Devrikh <devrikhjatav87@gmail.com>
2024-09-26 16:30:23 +0530

    scrambled lena Updated

--- Scrambled_Lena/scrambled_lena_sa.py
+++ Scrambled_Lena/scrambled_lena_sa.py
@@ -3,114 +3,105 @@ import math
 from PIL import Image
 import numpy as np
 
-def cost_function(puzzle):
-    cost = 0
-    for i in range(512):
-        for j in range(512):
-            if j + 1 != 512 and (j + 1) % 128 == 0:
-                cost += abs(int(puzzle[(512*i) + j]) - int(puzzle[(512*i) + j + 1]))
-            if i + 1 != 512 and (i + 1) % 128 == 0:
-                cost += abs(int(puzzle[(512*i) + j]) - int(puzzle[(512*(i+1)) + j]))
-    # print(cost)
-    return cost
+def calculate_cost(image_puzzle):
+    total_cost = 0
+    for row in range(512):
+        for col in range(512):
+            if col + 1 != 512 and (col + 1) % 128 == 0:
+                total_cost += abs(int(image_puzzle[(512 * row) + col]) - int(image_puzzle[(512 * row) + col + 1]))
+            if row + 1 != 512 and (row + 1) % 128 == 0:
+                total_cost += abs(int(image_puzzle[(512 * row) + col]) - int(image_puzzle[(512 * (row + 1)) + col]))
+    return total_cost
 
-def swap_pieces(puzzle):
-    i, j= random.sample(range(16), 2)
-    r1 = int(i/4)
-    r2 = int(j/4)
-    c1 = int(i%4)
-    c2 = int(j%4)
-    rn1 = int(128 * r1)
-    rn2 = int(128 * r2)
-    cn1 = int(128 * c1)
-    cn2 = int(128 * c2)
-    piece1 = []
-    piece2 = []
-    for i in range(128):
-        for j in range(128):
-            if((512*(rn1 + i)) + (cn1 + j) >= 262144):
-                print(i, j, rn1, cn1)
-            piece1.append(puzzle[(512*(rn1 + i)) + (cn1 + j)])
-    for i in range(128):
-        for j in range(128):
-            piece2.append(puzzle[(512*(rn2 + i)) + (cn2 + j)])
-    for i in range(128):
-        for j in range(128):
-            puzzle[(512*(rn1 + i)) + (cn1 + j)] = piece2[(i * 128) + j]
-    for i in range(128):
-        for j in range(128):
-            puzzle[(512*(rn2 + i)) + (cn2 + j)] = piece1[(i * 128) + j]
+def swap_pieces(image_puzzle):
+    idx1, idx2 = random.sample(range(16), 2)
+    row1, col1 = divmod(idx1, 4)
+    row2, col2 = divmod(idx2, 4)
+    start_row1, start_row2 = 128 * row1, 128 * row2
+    start_col1, start_col2 = 128 * col1, 128 * col2
 
-    return puzzle
+    piece_one = []
+    piece_two = []
 
-def simulated_annealing(puzzle, T_initial, alpha, stopping_temp):
-    minCost = 100000000
-    minState = []
-    c = 0
-    T = T_initial
-    current_state = puzzle
-    current_cost = cost_function(current_state)
+    for i in range(128):
+        for j in range(128):
+            piece_one.append(image_puzzle[(512 * (start_row1 + i)) + (start_col1 + j)])
+            piece_two.append(image_puzzle[(512 * (start_row2 + i)) + (start_col2 + j)])
     
-    while T > stopping_temp:
-        c = c + 1
+    for i in range(128):
+        for j in range(128):
+            image_puzzle[(512 * (start_row1 + i)) + (start_col1 + j)] = piece_two[(i * 128) + j]
+            image_puzzle[(512 * (start_row2 + i)) + (start_col2 + j)] = piece_one[(i * 128) + j]
+
+    return image_puzzle
+
+def simulated_annealing(initial_puzzle, initial_temp, cooling_rate, final_temp):
+    best_cost = float('inf')
+    best_state = []
+    temperature = initial_temp
+    current_state = initial_puzzle
+    current_cost = calculate_cost(current_state)
+    
+    while temperature > final_temp:
         new_state = swap_pieces(current_state.copy())
-        new_cost = cost_function(new_state)
-        # print(c, current_cost)
+        new_cost = calculate_cost(new_state)
+
         if new_cost < current_cost:
             current_state = new_state
             current_cost = new_cost
-            if(current_cost < minCost):
-                minCost = current_cost
-                minState = current_state.copy()
+            if current_cost < best_cost:
+                best_cost = current_cost
+                best_state = current_state.copy()
         else:
-            if random.uniform(0, 1) < math.exp((current_cost - new_cost) / T):
+            if random.uniform(0, 1) < math.exp((current_cost - new_cost) / temperature):
                 current_state = new_state
                 current_cost = new_cost
         
-        T *= alpha 
+        temperature *= cooling_rate 
     
-    return minState, minCost
+    return best_state, best_cost
 
 
 def display_image_from_text(file_path):
-    image_data = []
+    pixel_values = []
     with open(file_path, 'r') as file:
         for line in file:
-            numbers = line.split()
-            image_data.extend(int(num) for num in numbers)
+            pixel_values.extend(int(num) for num in line.split())
 
-    image_array = np.array(image_data, dtype=np.uint8).reshape((512, 512))
-    img = Image.fromarray(image_array, mode='L')
+    pixel_array = np.array(pixel_values, dtype=np.uint8).reshape((512, 512))
+    img = Image.fromarray(pixel_array, mode='L')
     img.show()
 
 
 
-puzzle = []
+
+image_puzzle = []
 with open('scrambled_lena.mat', 'r') as file:
     for line in file:
-        puzzle.extend(line.split())
+        image_puzzle.extend(line.split())
 
-assert len(puzzle) == 512 * 512, "Puzzle data is not the correct size!"
+assert len(image_puzzle) == 512 * 512, "Puzzle data does not match expected size!"
 
-ans = []
-minCost = float('inf') 
-for i in range(5):
-    T_initial = 1000
-    alpha = 0.99
-    stopping_temp = 0.1
-    solved_puzzle, cost = simulated_annealing(puzzle, T_initial, alpha, stopping_temp)
+final_solution = []
+best_cost = float('inf') 
+
+for attempt in range(5):
+    initial_temp = 1000
+    cooling_rate = 0.99
+    final_temp = 0.1
+    solved_puzzle, cost = simulated_annealing(image_puzzle, initial_temp, cooling_rate, final_temp)
     
-    if cost < minCost:
-        minCost = cost
-        puzzle = solved_puzzle.copy()
-        ans = puzzle
+    if cost < best_cost:
+        best_cost = cost
+        image_puzzle = solved_puzzle.copy()
+        final_solution = image_puzzle
     print(cost)
-assert len(ans) == 262144, "Output data is not the correct size!"
 
-with open('ans.mat', 'w') as file:
-    for item in ans:
-        file.write(f"{item}\n") 
+assert len(final_solution) == 262144, "Output data does not match expected size!"
 
+# Write the output data
+with open('ans.mat', 'w') as output_file:
+    for value in final_solution:
+        output_file.write(f"{value}\n") 
 
-        
 display_image_from_text('ans.mat')
